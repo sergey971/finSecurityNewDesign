39ea53f2ca95dfbeb9b02c00a8eb6eae015833f0
murin.89@list.ru <murin.89@list.ru>
2023-09-01 16:00:31 +0400

    add new file

--- js/main.js
+++ js/main.js
@@ -5,11 +5,22 @@ const partnersTitle = document.querySelector('.partners__title');
 const partnersBtn = document.querySelector('.partners__btn');
 
 // headerNavRow.classList.add('none');
-burger.addEventListener('click', () =>{
+burger.addEventListener('click', () => {
     burger.classList.toggle('open');
     partnersTitle.classList.toggle('none');
     partner.classList.toggle('partnerBG');
     partnersBtn.classList.toggle('partnersBtn');
     headerNavRow.classList.toggle('active');
-    
+
+});
+
+
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    // prevArrow: $('.prev'), // Кастомная кнопка "Предыдущий слайд"
+    nextArrow: $('.next')  // Кастомная кнопка "Следующий слайд"
 });
